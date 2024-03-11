7f9fd27d216f6871e98f6af24717634def8905f7
shrutirrsharma <130732457+shrutirrsharma@users.noreply.github.com>
2024-03-11 13:47:38 +0530

    dnchk.js

--- dnchk.js
+++ dnchk.js
@@ -2,10 +2,12 @@ const mysql = require('mysql');
 
 // Create a connection to the MySQL database
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: "a22f924f0f9fa4f779d92c4f2c291373-242620234.ap-south-1.elb.amazonaws.com",
     user: "root",
-    password: "Root@123",
+    password: "Shruti@991001",
+    port: 3306
     database: 'pharmacy'
+    connectionLimit: 10, // Adjust the connection pool size as needed
 });
 
 // Define a function that executes a query and returns the results
